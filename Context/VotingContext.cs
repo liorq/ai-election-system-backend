07256e571104012a04d2d750a38a0a864af1eba0
lior daniel <liorqm@gmail.com>
2024-10-15 23:42:54 +0300

    first commit

---
+++ Context/VotingContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using WebApplication16.models;
+
+namespace VotingApp.Data
+{
+    public class VotingContext : DbContext
+    {
+        public VotingContext(DbContextOptions<VotingContext> options)
+               : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.FaceEmbedding)
+                .HasConversion(
+                    v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+                    v => JsonSerializer.Deserialize<float[]>(v, (JsonSerializerOptions)null));
+        }
+    }
+}
